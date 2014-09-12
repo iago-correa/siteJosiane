718574397e3de7da96af2480e5ed4ae5b047e255
root <iago.gc.eliote@gmail.com>
2014-09-12 08:54:27 -0300

    Correção no modelo aluno

--- siteJosiane/app/models/aluno.rb
+++ siteJosiane/app/models/aluno.rb
@@ -12,15 +12,14 @@ class Aluno < ActiveRecord::Base
 		uniqueness:{message: "Matrícula já cadastrada"},
 		numericality:{message: "Matrícula inválida"}
 	validates :senha, presence: {message: "Senha não pode ficar em branco"}
-	validates :confirmado, presence: {message: "É necessário que o aluno seja ou não confirmado"},
+	validates :confirmado, presence: {message: "É necessário que o aluno seja ou não confirmado"}
+	validates :email, presence: {message: "Email não pode ficar em branco"},
 		uniqueness:{message: "Email já cadastrado"}
 
 	validate :email?
 
   	private def email?
-  		if email != nil
-  			errors.add("Email inválido") unless email =~ /[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/
-  		end
+		errors.add("Email inválido") unless email =~ /[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/
   	end
 
 end
