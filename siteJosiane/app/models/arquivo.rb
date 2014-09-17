1b4af732a9fad89fbd91e2c2812744db934abf37
Iago <iago.gc.eliote@gmail.com>
2014-09-17 02:14:51 -0300

    Retificação de models arquivo, comentario e notificacao

--- siteJosiane/app/models/arquivo.rb
+++ siteJosiane/app/models/arquivo.rb
@@ -2,12 +2,12 @@
 
 class Arquivo < ActiveRecord::Base
 
-	belongs_to :posts
+	belongs_to :post
 
 	validates_associated :post
 
 	validates :post_id, presence: {message: "Não é possível existir um arquivo sem um post associado"}
-	validates :conteudo, presence: {message: "Conteúdo não pode ficar em branco"},
+	validates :nome, presence: {message: "Conteúdo não pode ficar em branco"},
 		uniqueness:{message: "Conteúdo já postado"}
 	validates :extensao, presence: {message: "Extensão não pode ficar em branco"}
 
