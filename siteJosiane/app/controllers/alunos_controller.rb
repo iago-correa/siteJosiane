a7a92bd5ff5739136955d3d071e2bd87c89468d8
Iago <iago.gc.eliote@gmail.com>
2014-10-10 21:52:47 -0300

    #78

--- siteJosiane/app/controllers/alunos_controller.rb
+++ siteJosiane/app/controllers/alunos_controller.rb
@@ -7,77 +7,100 @@ class AlunosController < ApplicationController
 	end
 
 	def new
-		@aluno = Aluno.new
+		if not session[:usuario]
+			@aluno = Aluno.new
+		else
+			redirect_to root_path
+		end
 	end
 
 	def create
 		
-		@aluno = Aluno.new params.require(:aluno).permit(:nome, :matricula, :senha, :email, :turma_id)
-		senha2 = params[:senha2]
-		
-		if @aluno.senha.eql? senha2
+		if not session[:usuario]
+			
+			@aluno = Aluno.new params.require(:aluno).permit(:nome, :matricula, :senha, :email, :turma_id)
+			senha2 = params[:senha2]
+			
+			if @aluno.senha.eql? senha2
 
-	  		@aluno.confirmado = false
-	  		@aluno.senha = Digest::MD5.hexdigest(@aluno.senha)
-	  		
-	  		if @aluno.save
-	  			redirect_to @aluno, notice: "Bem vindo(a) #{@aluno.nome}!"
+		  		@aluno.confirmado = false
+		  		@aluno.senha = Digest::MD5.hexdigest(@aluno.senha)
+		  		
+		  		if @aluno.save
+		  			redirect_to @aluno, notice: "Bem vindo(a) #{@aluno.nome}!"
+		  		else
+		  			
+		  			message = "Falha no cadastro: "
+
+		  			@aluno.errors.full_messages.each do |m|
+						message += m
+				    end
+
+				    flash[:alert] = message
+	        		render 'new'
+
+		  		end
+
 	  		else
-	  			
-	  			message = "Falha no cadastro: "
 
-	  			@aluno.errors.full_messages.each do |m|
-					message += m
-			    end
-
-			    flash[:alert] = message
-        		render 'new'
+	  			flash[:alert] = "Senhas não verificam"
+	        	render 'new'
 
 	  		end
 
-  		else
-
-  			flash[:alert] = "Senhas não verificam"
-        	render 'new'
-
-  		end
+	  	else
+			redirect_to root_path
+		end
 
 	end
 
 	def edit
 
 		@aluno = Aluno.find(params[:id])
+		if not session[:usuario] && @aluno.matricula==session[:usuario]
+			redirect_to root_path
+		end
 
 	end
 
 	def update
 
 		@aluno = Aluno.find(params[:id])
+		if session[:usuario] && @aluno.matricula==session[:usuario]
 		
-		if @aluno.update(params.require(:aluno).permit(:nome, :matricula, :email, :turma_id))
-            redirect_to @aluno, notice: "Alterações efetivadas com sucesso"
-        else
+			if @aluno.update(params.require(:aluno).permit(:nome, :matricula, :email, :turma_id))
+	            redirect_to @aluno, notice: "Alterações efetivadas com sucesso"
+	        else
 
-        	message = "Falha nas alterações: "
+	        	message = "Falha nas alterações: "
 
-	  		@aluno.errors.full_messages.each do |m|
-				message += m
-			   end
+		  		@aluno.errors.full_messages.each do |m|
+					message += m
+				   end
 
-	  		redirect_to edit_aluno_path, notice: message
-            
-        end
+		  		redirect_to edit_aluno_path, notice: message
+	            
+	        end
+
+	    else
+			redirect_to root_path
+	    end
 
 	end
 
 	def show
 
 		@aluno = Aluno.find(params[:id])
+		if not session[:usuario] && @aluno.matricula==session[:usuario]
+			redirect_to root_path
+		end
 
 	end
 
 	def destroy
 
+		#Como so o professor deleta o aluno, necessario
+		#verificar login desse
         @aluno = Aluno.find(params[:id])
         if @aluno.destroy
         	redirect_to :alunos, notice: "Aluno #{@aluno.nome} foi excluído"
@@ -90,6 +113,8 @@ class AlunosController < ApplicationController
 
 	def aprovar
 
+		#Como so o professor aprova o cadastro do aluno
+		#necessario verificar login desse
 		@aluno = Aluno.find(params[:id])
 
 		if @aluno.update(confirmado: true)
@@ -103,30 +128,37 @@ class AlunosController < ApplicationController
 	def nova_senha
 
 		@aluno = Aluno.find(params[:id])
+		if not session[:usuario] && @aluno.matricula==session[:usuario]
+			redirect_to root_path
+		end
 
 	end
 
 	def troca_senha
 
 		@aluno = Aluno.find(params[:id])
-		old_senha = Digest::MD5.hexdigest(params[:senha_antiga])
+		if session[:usuario] && @aluno.matricula==session[:usuario]
+			old_senha = Digest::MD5.hexdigest(params[:senha_antiga])
 
-		if @aluno.senha.eql? old_senha
-	  		
-			new_senha = Digest::MD5.hexdigest(params[:senha])
+			if @aluno.senha.eql? old_senha
+		  		
+				new_senha = Digest::MD5.hexdigest(params[:senha])
 
-	  		if @aluno.update(senha: new_senha)
-	  			redirect_to @aluno, notice: "Troca de senha efetuada com sucesso"
+		  		if @aluno.update(senha: new_senha)
+		  			redirect_to @aluno, notice: "Troca de senha efetuada com sucesso"
+		  		else
+		  			redirect_to :nova_senha, notice: "Falha na troca de senha"
+		  		end
+
 	  		else
-	  			redirect_to :nova_senha, notice: "Falha na troca de senha"
+
+	  			redirect_to :nova_senha, notice: "Senha antiga incorreta"
+
 	  		end
 
-  		else
-
-  			redirect_to :nova_senha, notice: "Senha antiga incorreta"
-
-  		end
-
+	  	else
+			redirect_to root_path
+	    end
 
 	end
 
