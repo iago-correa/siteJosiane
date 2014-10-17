6e0c67daedee3fa9990055aabcbf352cdc98db7d
carolissp <aanapacheco@hotmail.com>
2014-10-17 11:12:21 -0300

    #55

--- siteJosiane/app/controllers/professor_controller.rb
+++ siteJosiane/app/controllers/professor_controller.rb
@@ -1,5 +1,35 @@
+# encoding: UTF-8
+
 class ProfessorController < ApplicationController
 
+	def atendimento
 
+		
+
+	end
+
+	def arquivo
+
+
+
+	end
+
+	def disciplina
+
+
+
+	end
+
+	def nota
+
+
+
+	end
+
+	def presenca
+
+
+
+	end
 
 end
