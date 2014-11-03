226cc075dc3152b951c7f7e1ba8500699d31f692
Iago <iago.gc.eliote@gmail.com>
2014-11-02 22:32:29 -0200

    #105

--- siteJosiane/app/controllers/home_controller.rb
+++ siteJosiane/app/controllers/home_controller.rb
@@ -8,26 +8,38 @@ class HomeController < ApplicationController
 
   	def login
 
-		@aluno = Aluno.new
+  		if not session[:usuario]
+			@aluno = Aluno.new
+		else
+			redirect_to root_path
+		end
 
 	end
 
 	def log
 		
-		@aluno = Aluno.find_by_matricula_and_senha(params[:matricula],Digest::MD5.hexdigest(params[:senha]))	
+		if not session[:usuario]
+
+		@aluno = Aluno.find_by_matricula_and_senha(params[:matricula],Digest::MD5.hexdigest(params[:senha]))
 		
-		if !@aluno.nil?
-			if @aluno.confirmado
-				session[:usuario] = @aluno.matricula
-				redirect_to :home
-		  	else
-		  		flash.now[:alert] = "Ainda em espera de aprovação de cadastro"
-        		render :login
-		  	end
+			if !@aluno.nil?
+				if @aluno.confirmado
+					session[:usuario] = @aluno.matricula
+					redirect_to :home
+			  	else
+			  		flash.now[:alert] = "Ainda em espera de aprovação de cadastro"
+	        		render :login
+			  	end
+			else
+				@aluno = Aluno.new
+				flash.now[:alert] = 'Matrícula ou senha incorretas'
+	        	render :login
+			end
+
 		else
-			@aluno = Aluno.new
-			flash.now[:alert] = 'Matrícula ou senha incorretas'
-        	render :login
+
+			redirect_to root_path
+			
 		end
 
 	end
