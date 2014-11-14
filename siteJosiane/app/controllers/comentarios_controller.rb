2e520d93f9016ad321aebeef1c6382ffba2950fe
Iago <iago.gc.eliote@gmail.com>
2014-11-14 00:50:23 -0200

    #120

--- siteJosiane/app/controllers/comentarios_controller.rb
+++ siteJosiane/app/controllers/comentarios_controller.rb
@@ -1,2 +1,44 @@
+# encoding: UTF-8
+
 class ComentariosController < ApplicationController
+
+	def new
+		if session[:usuario]
+			@comentario = Comentario.new
+		else
+			redirect_to root_path
+		end
+	end
+
+	def create
+		
+		if session[:usuario]
+			
+			usuario = Aluno.find_by_matricula(session[:usuario])
+			post = Post.find(session[:post])
+
+			@comentario = Comentario.new params.require(:comentario).permit(:conteudo)
+			@comentario.post_id = post.id
+			@comentario.aluno_id = usuario.id
+
+			if @comentario.save
+				redirect_to @post, notice: "Comentário realizado"
+			else		
+				message = "Falha no comentário: "
+				@comentario.errors.full_messages.each do |m|
+					message += m
+				end
+
+				flash.now[:alert] = message
+	    		render 'new'
+  			end
+
+	  	else
+
+			redirect_to root_path
+
+		end
+
+	end
+
 end
